ca5883a0588c4882f3836ba647e5250ecea1eaa1
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-02 00:31:43 -0400

    updated app.js routes and installed middleware for project

--- app.js
+++ app.js
@@ -3,42 +3,42 @@ const app = express();
 const PORT = 3000;
 const morgan = require('morgan');
 const path = require('node:path');
+const methodOverride = require('method-override');
+const routes = require('./routes/index');
 
+app.use(morgan('combined'));
+app.use(methodOverride('method'));
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (request, response) => {
-    response.render('pages/index');
-});
 
-app.get('/login', (request, response) => {
-    response.render('pages/login');
-});
+// app.get('/reviews', (request, response) => {
+//     response.render('pages/reviews');
+// });
 
-app.get('/reviews', (request, response) => {
-    response.render('pages/reviews');
-});
+// app.get('/create-review', (request, response) => {
+//     response.render('pages/create-review');
+// });
 
-app.get('/create-review', (request, response) => {
-    response.render('pages/create-review');
-});
+// app.get('/category-food', (request, response) => {
+//     response.render('pages/category-food');
+// });
 
-app.get('/category-food', (request, response) => {
-    response.render('pages/category-food');
-});
+// app.get('/category-retail', (request, response) => {
+//     response.render('pages/category-retail');
+// });
 
-app.get('/category-retail', (request, response) => {
-    response.render('pages/category-retail');
-});
+// app.get('/review-slutty-vegan', (request, response) => {
+//     response.render('pages/review-slutty-vegan');
+// });
 
-app.get('/review-slutty-vegan', (request, response) => {
-    response.render('pages/review-slutty-vegan');
-});
+// app.get('/review-nourish-and-bloom', (request, response) => {
+//     response.render('pages/review-nourish-and-bloom');
+// });
 
-app.get('/review-nourish-and-bloom', (request, response) => {
-    response.render('pages/review-nourish-and-bloom');
-});
-
+// Server
 app.listen(PORT, () => {
     console.log(`The server is listening on port ${PORT}`);
 });
