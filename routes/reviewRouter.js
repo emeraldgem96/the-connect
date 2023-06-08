d6884ee76640e9d17910a6611f4801e3af78e3b6
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-08 01:52:44 -0400

    updated review router and removed paths that went under admin

--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -4,15 +4,9 @@ const router = express.Router();
 
 // Review Routes
 router.route('/')
+    .get(reviewController.reviews)
+
+router.route('/:_id')
     .get(reviewController.review)
-
-router.route('/create-review')
-    .get(reviewController.create_review)
-
-router.route('/create-business')
-    .get(reviewController.business_create)
-
-// router.route('/:_id')
-//     .get(reviewController.review)
 
 module.exports = router;
