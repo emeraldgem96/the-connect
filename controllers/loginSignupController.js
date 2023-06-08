34a770840d9b0ff011a597ede1ff306578c489c7
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-08 01:54:58 -0400

    set up the login handlers in controller

--- controllers/loginSignupController.js
+++ controllers/loginSignupController.js
@@ -3,5 +3,11 @@ const User = require('../models/userModel')
 module.exports = {
     login_signup: (request, response) => {
         response.render('pages/login');
+    },
+    login_post: (request, response) => {
+        
+    },
+    register_post: (request, response) => {
+
     }
 }
