298cf34d30726646918b82db8d83afbbd51fe5ec
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-08 01:53:17 -0400

    created all the login and register routes

--- routes/loginSignupRouter.js
+++ routes/loginSignupRouter.js
@@ -5,5 +5,9 @@ const router = express.Router();
 // Login & Sign-up Routes
 router.route('/')
     .get(loginSignupController.login_signup)
+    .post(loginSignupController.login_post)
+
+router.route('/register')
+    .post(loginSignupController.register_post)
 
 module.exports = router;
