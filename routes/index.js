0640f64544af34175dbc244fd9f5245c9b40c0f4
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-08 01:48:49 -0400

    added admin route to index router

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const siteRouter = require('../routes/siteRouter');
 const reviewRouter = require('../routes/reviewRouter')
 const loginSignupRouter = require('../routes/loginSignupRouter');
+const adminRouter = require('../routes/adminRouter');
 const router = express.Router();
 
 router.use('/', siteRouter);
 router.use('/reviews', reviewRouter);
 router.use('/login', loginSignupRouter);
+router.use('/admin', adminRouter);
 
 module.exports = router;
