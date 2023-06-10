4c5e3f40d9ba2853338e3aed537041a8ee2e5003
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-10 08:20:59 -0400

    added passport to user model

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,30 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
-    username: String,
-    password: String,
+    username: {
+        String,
+    },
+    password: {
+        String,
+    }
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+// Amen for stack overflow for this code, will need to be updated
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
 
 module.exports = User;
