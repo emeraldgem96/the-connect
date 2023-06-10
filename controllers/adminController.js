f98876f75b44df63b253325b8e49e88d5644e0c5
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-09 21:55:21 -0400

    added notes and outline for admin ctrl, set-up review ctrl to display each review based on database ID

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,13 +7,23 @@ module.exports = {
         response.render('pages/create-review')
     }, 
     review_create_post: (request, response) => {
+        // Create operation from CS Comics 
+        response.redirect()
+        // need to redirect to a conf. page
 
     },
     business_create: (request, response) => {
         response.render('pages/create-business')
     },
     business_create_post: (request, response) => {
-        
+        // Create operation from CS Comics
+        response.redirect()
+        // need to redirect to a conf. page
+
+    },
+    logout: (request, response) => {
+        // request.logout();
+        // response.redirect('/login');
     }
 }
 
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,14 +1,19 @@
 const { request, response } = require("express");
 const Review = require('../models/reviewModel');
 const Business = require('../models/businessModel');
+const {v4: uuid} = require('uuid')
 
 module.exports = {
     reviews: (request, response) => {
         response.render('pages/reviews')
         // displays the whole reviews page
     },
-    review: (request, response) => {
+    review_get: (request, response) => {
         // displays each indivdual review page
-        
+        let id = request.params._id;
+        const foundReview = Review.find(review => review._id === String(id));
+        response.render('pages/review-template', {
+            reviewItem: foundReview
+        })
     }
 }
