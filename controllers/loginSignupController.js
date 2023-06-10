cf05cc1a054d19910da634083d6ba4ee051a2791
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-10 08:23:16 -0400

    used passport to setup security for login and registration

--- controllers/loginSignupController.js
+++ controllers/loginSignupController.js
@@ -1,3 +1,4 @@
+const passport = require('passport');
 const User = require('../models/userModel')
 
 module.exports = {
@@ -5,9 +6,31 @@ module.exports = {
         response.render('pages/login');
     },
     login_post: (request, response) => {
-        
+        const user = new User({
+            username: request.body.username,
+            password: request.body.password
+        });
+        request.login(user, (error) => {
+            if (error) {
+                return error;
+            } else {
+                passport.authenticate('local')(request, response, () => {
+                    response.redirect('/admin')
+                })
+            }
+        })
     },
     register_post: (request, response) => {
-
+        User.register({username: request.body.username}. request.body.password, (error, user) => {
+            if (error) {
+                console.log(error);
+                response.redirect('/register');
+            } else {
+                passport.authenticate('local')(request, response, () => {
+                response.redirect('/login');  
+                })
+            }
+        })
+        
     }
 }
