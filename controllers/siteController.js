352290ec0c3f2688cd42140217195d8c42d4a897
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-11 14:43:21 -0400

    added the reviews main pg handler function to site ctrl

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -1,7 +1,20 @@
 const { request } = require("express");
+const Business = require('../models/businessModel');
 
 module.exports = {
     index: (request, response) => {
         response.render('pages/index');
-    }
+    },
+    reviews: async (request, response) => {
+        try {
+          const allBusinesses = await Business.find({}).exec();
+          response.render('pages/reviews', {
+            businessArray: allBusinesses
+          });
+        } catch (error) {
+          // Handle error
+          console.error(error);
+          response.status(500).send('Internal Server Error');
+        }
+      },
 }
