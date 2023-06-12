84b1bc53ab1c1ad55e4304a4cc0a064888f9bce0
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-12 17:13:15 -0400

    added site validation for business and reviews model

--- models/businessModel.js
+++ models/businessModel.js
@@ -2,10 +2,30 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const businessSchema = new Schema({
-    business_name: String,
-    category: String,
-    description: String,
-    location: String,
+    business_name: {
+        type: String,
+        required: [true, 'A business name is required.'],
+        minlength: [1, 'Minimun length for the business\'s name is 1 character.']
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: [true, 'A description is required.'],
+        minlength: [5, 'Minimum length for the description is 5 characters.']
+    },
+    location_address: {
+        type: String,
+        required: [true, 'An address is required.'],
+        minlength: [5, 'Minimum length for the location is 5 characters.']
+    },
+    location_city: {
+        type: String,
+        required: [true, 'A city and state is required.'],
+        minlength: [5, 'Minimum length for the city and state is 5 characters.']
+    }, 
     website: String
 });
 
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -2,10 +2,20 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const reviewSchema = new Schema({
-    review_title: String,
-    review_comment: String,
-    rating: Number,
-    recommend: String
+    review_title: {
+        type: String,
+        required: [true, 'A review title is required.'],
+        maxlength: [50, 'Maximum length for the review title is 50 characters.']
+    },
+    review_comment: {
+        type: String,
+        required: [true, 'A review comment is required.'],
+        minlength: [50, 'The minimum length of the comment is 50 characters.']
+    },
+    recommend: {
+        type: String,
+        required: true
+    }
 });
 
 const Review = mongoose.model('Review', reviewSchema);
