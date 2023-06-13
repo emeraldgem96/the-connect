146772fe57d7fe117762b2dfda660b5e126c8e9b
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-13 01:40:43 -0400

    removed business model and section bc its now merged in with reviews

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -1,20 +1,20 @@
 const { request } = require("express");
-const Business = require('../models/businessModel');
+const Review = require('../models/reviewModel');
 
 module.exports = {
     index: (request, response) => {
         response.render('pages/index');
     },
     reviews: async (request, response) => {
-        try {
-          const allBusinesses = await Business.find({}).exec();
-          response.render('pages/reviews', {
-            businessArray: allBusinesses
-          });
-        } catch (error) {
-          // Handle error
-          console.error(error);
-          response.status(500).send('Internal Server Error');
-        }
-      },
+      try {
+        const allReviews = await Review.find({});
+        response.render('pages/reviews', {
+          reviewArray: allReviews
+        });
+      } catch (error) {
+        console.error(error);
+        // Handle the error, such as displaying an error page or sending an error response
+        response.status(500).send('Internal Server Error');
+      }
+    }    
 }
