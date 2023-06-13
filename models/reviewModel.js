0944865ac2c3948b31ba493a1ba0512e330e323b
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-13 01:44:58 -0400

    add form validation and combined parts of business form with reviews form

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -2,15 +2,37 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const reviewSchema = new Schema({
+    business_name: {
+        type: String,
+        required: [true, 'A business name is required.'],
+        minlength: [1, 'Minimun length for the business\'s name is 1 character.']
+    },
+    business_address: {
+        type: String,
+        required: [true, 'An address is required.'],
+        minlength: [5, 'Minimum length for the location is 5 characters.']
+    },
+    website: {
+        type: String,
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: [true, 'The rating is required.'],
+        min: [1, 'Minimun number of rating is 1']
+    },
     review_title: {
         type: String,
         required: [true, 'A review title is required.'],
         maxlength: [50, 'Maximum length for the review title is 50 characters.']
     },
-    review_comment: {
+    review_blurb: {
         type: String,
-        required: [true, 'A review comment is required.'],
-        minlength: [50, 'The minimum length of the comment is 50 characters.']
+        required: [true, 'A review blurb is required.'],
+        minlength: [50, 'The minimum length of your blurb is 50 characters.']
     },
     recommend: {
         type: String,
