720a30562009ac15c60737f40ee1a8eef0dde59d
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-13 01:42:47 -0400

    coded out review ctrl for later feature work

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const siteRouter = require('../routes/siteRouter');
-const reviewRouter = require('../routes/reviewRouter')
+// const reviewRouter = require('../routes/reviewRouter')
 const loginSignupRouter = require('../routes/loginSignupRouter');
 const adminRouter = require('../routes/adminRouter');
 const router = express.Router();
 
 router.use('/', siteRouter);
-router.use('/review', reviewRouter);
+// router.use('/review', reviewRouter);
 router.use('/login', loginSignupRouter);
 router.use('/admin', adminRouter);
 
