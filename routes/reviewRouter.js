dfedcfe5d8a4776070d8c44205152c8a675718f7
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-13 01:41:36 -0400

    moved to hidden folder for features

--- routes/reviewRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const reviewController = require('../controllers/reviewController');
-const router = express.Router();
-
-// Review Routes
-router.route('/details')
-    .get(reviewController.review_get)
-
-module.exports = router;
