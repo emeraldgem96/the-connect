c502cac941af91f8a186ba6e6ed528856ebe5c11
emeraldgem96 <emeraldgempr@gmail.com>
2023-06-15 02:13:04 -0400

    added scroll to top functionality and check password to block access to users to a creator only page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,4 @@
-const { response } = require("express");
+// const { response } = require("express");
 
 // Hide/Unhide Create Account button
 function createAccount() {
@@ -10,8 +10,9 @@ function createAccount() {
     }
 }
 
-createAccount();
+// createAccount();
 
+// Blocked button for create review on reviews page
 function blockedButton() {
     let block;
     if(confirm('This is out of service. Please login or create an account to create a review.')) {
@@ -20,4 +21,34 @@ function blockedButton() {
     document.getElementById('blocked-button').disabled = true;
 }
 
-blockedButton();
+// blockedButton();
+
+// Password Protected button
+function checkPassword() {
+    let password = prompt("CREATOR ONLY! Please enter the password to access this page.");
+    if (password === 'creatorImani96') {
+        alert('Password is correct. Access granted!');
+        window.location = "/admin/change-review"
+    } else {
+        alert('Incorrect password. Access denied!');
+        window.location = "/admin"
+    }
+}
+
+// checkPassword();
+
+let myButton = document.getElementById("myButton");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
